a21b01230e916ce9dee11cd0750e2907985fe78e
Paul Bryan <pabryan@gmail.com>
2015-11-07 10:54:01 +0100

    Show where biblatex.sty is actually installed to see if it's in the path.

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cp ./index.html ./"${BUILD_DIR}"
 
 dpkg -L texlive-binaries | grep kpsewhich
 kpsewhich -show-path .sty
+dpkg -L texlive-bibtex-extra | grep biblatex.sty
 
 kpsewhich biblatex.sty
 echo "Success on kpsewhich"
