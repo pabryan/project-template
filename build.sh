343f4b7a109e1aa20f38914edeaaa387837f9fe7
Paul Bryan <pabryan@gmail.com>
2015-11-07 12:38:51 +0100

    Removed debugging tests to find biblatex.sty.

--- build.sh
+++ build.sh
@@ -6,21 +6,5 @@ set -e
 mkdir "${BUILD_DIR}"
 cp ./index.html ./"${BUILD_DIR}"
 
-uname -a
-echo "***** Do we have kpsewhich?"
-dpkg -L texlive-binaries | grep kpsewhich
-
-echo "***** Where do we find .sty files?"
-kpsewhich -show-path .sty
-
-echo "***** What files does texlive-bibtex-extra contain? Where is biblatex.sty?"
-dpkg -L texlive-bibtex-extra 
-
-echo "***** Try find biblatex system wise?"
-find /usr/share/texlive/texmf-dist/tex/latex -iname 'biblatex*'
-
-kpsewhich biblatex.sty
-echo "Success on kpsewhich"
-
 # Build LaTeX files
 latexmk
