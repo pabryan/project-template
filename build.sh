7c43daf426d8c2d78ea35b8048fbfac8601987ba
Paul Bryan <pabryan@gmail.com>
2015-11-07 11:06:57 +0100

    Added lots of debugging output to help find the missing biblatex.sty file.

--- build.sh
+++ build.sh
@@ -6,9 +6,18 @@ set -e
 mkdir "${BUILD_DIR}"
 cp ./index.html ./"${BUILD_DIR}"
 
+uname -a
+echo "***** Do we have kpsewhich?"
 dpkg -L texlive-binaries | grep kpsewhich
+
+echo "***** Where do we find .sty files?"
 kpsewhich -show-path .sty
+
+echo "***** Do we have biblatex.sty?"
 dpkg -L texlive-bibtex-extra | grep biblatex.sty
+
+echo "***** Try find it system wise?"
+find /usr/share/texlive/texmf-dist/tex/latex -iname 'biblatex*'
 
 kpsewhich biblatex.sty
 echo "Success on kpsewhich"
