39916554786d1c289a5db13f13cc4672441013d9
Paul Bryan <pabryan@gmail.com>
2015-11-07 10:47:27 +0100

    More kpsewhich debugging showing paths.

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ set -e
 mkdir "${BUILD_DIR}"
 cp ./index.html ./"${BUILD_DIR}"
 
+dpkg -L texlive-binaries | grep kpsewhich
+kpsewhich -show-path .sty
+
 kpsewhich biblatex.sty
+echo "Success on kpsewhich"
 
 # Build LaTeX files
 latexmk
